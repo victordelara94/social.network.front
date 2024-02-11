bc2ae8b2c4310d5379b6a35b293fde54a5012d4c
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2024-02-11 14:39:41 +0100

    Add check for private or public

--- src/components/user.detail/user.detail.tsx
+++ src/components/user.detail/user.detail.tsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 import { usePosts } from '../../hooks/use.posts';
 import { useUsers } from '../../hooks/use.users';
 import { User } from '../../models/user.model';
+import Switch from '../switch/switch';
 import styles from './user.detail.module.scss';
-
 const UserDetail = () => {
-  const { actualUser, searchedUser } = useUsers();
+  const { actualUser, searchedUser, updateUser } = useUsers();
   const {
     searchOwnPosts,
     postState: { userPosts },
@@ -21,8 +21,20 @@ const UserDetail = () => {
       searchOwnPosts(userToDetail.id);
     }
   }, [searchedUser, actualUser, userToDetail, searchOwnPosts]);
+  const handleChange = (isChecked: boolean) => {
+    updateUser(actualUser.user.id, actualUser.token, { isPrivate: isChecked });
+  };
   return (
     <section>
+      {!searchedUser.id && (
+        <div className={styles['private']}>
+          <span>Public</span>
+          <Switch
+            isPrivate={actualUser.user.isPrivate}
+            onChange={handleChange}
+          ></Switch>
+        </div>
+      )}
       <div className={styles['userInfo']}>
         <img />
         <span></span>
